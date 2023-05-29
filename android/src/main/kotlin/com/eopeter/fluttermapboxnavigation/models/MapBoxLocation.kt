554086afd6f09622db3a6e31144d81a4685a8a3a
Emmanuel Oche <eopeter@gmail.com>
2023-05-29 12:13:14 -0400

    rename android package

--- android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/models/MapBoxLocation.kt
+++ android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/models/MapBoxLocation.kt
@@ -1,5 +1,7 @@
 package com.eopeter.fluttermapboxnavigation.models
 
+class MapBoxLocation(val name: String = "", val latitude: Double?, val longitude: Double?) {
+
 class MapBoxLocation(val name: String = "", private val latitude: Double?, private val longitude: Double?) {
     override fun toString(): String {
         return "{" +
